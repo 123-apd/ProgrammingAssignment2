3f846a600811283550f56a3b1d7742647a42d369
apd <arnepetterdrageset@gmail.com>
2014-11-13 11:23:12 +0100

    m

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,14 @@
 
 ## First let's see if we can get this up and running
 
+
+# set up a test matrix
+tvec <- c(1,1,1,2,2,2,1,1,1)
+
+trix <- matrix(tvec,3,3)
+
+trix2 <- solve(trix)
+
 makeCacheMatrix <- function(x = matrix()) {
 
 }
